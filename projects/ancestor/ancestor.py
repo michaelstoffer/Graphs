ff94cca6adcf61c7db02859a419daaefe5cf35df
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-07-25 14:37:00 -0400

    Completed Earliest Ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,22 @@
-
+from util import Queue
 def earliest_ancestor(ancestors, starting_node):
-    pass
+	queue = Queue()
+	cur_node = starting_node
+	cache = {}
+	for node in ancestors:
+		if node[1] not in cache:
+			cache[node[1]] = set()
+		cache[node[1]].add(node[0])
+
+	if starting_node in cache:
+		queue.enqueue(cache[cur_node])
+	else:
+		return -1
+
+	while True:
+		relations = queue.dequeue()
+		cur_node = min(relations)
+		if cur_node not in cache:
+			return cur_node
+		else:
+			queue.enqueue(cache[cur_node])
